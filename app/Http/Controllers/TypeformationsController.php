24217e7ffa7dcaf663b5d0ba66cd1fb633ecbddf
annoniyous <bouysantiman.y@outlook.com>
2021-02-23 12:07:57 +0100

    Cmrsf fini

---
+++ app/Http/Controllers/TypeformationsController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Typeformations;
+use Illuminate\Http\Request;
+
+class TypeformationsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Typeformations  $typeformations
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Typeformations $typeformations)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Typeformations  $typeformations
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Typeformations $typeformations)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Typeformations  $typeformations
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Typeformations $typeformations)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Typeformations  $typeformations
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Typeformations $typeformations)
+    {
+        //
+    }
+}
